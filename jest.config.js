e878e26dbe52ed7492a46bef82a99b36bcd097c5
vaibhav <vaibhavva219@gmail.com>
2024-04-18 02:46:30 +0530

    Added all files

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   preset: 'react-native',
+// };
+module.exports = {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
